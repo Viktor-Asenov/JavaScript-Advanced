1d9eaae75c179e9ce96f9c099b1789ee05444baa
Viktor-Asenov <79524317+Viktor-Asenov@users.noreply.github.com>
2022-05-23 18:49:35 +0300

    Solutions add

--- 05. DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
+++ 05. DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    console.log('TODO:...');
+    let input = document.getElementById('email');
+    input.addEventListener('change', onChange);
+
+    function onChange(ev){
+        const pattern = /^[a-z]+@[a-z]+\.[a-z]+$/;
+        if (!pattern.test(ev.target.value)) {
+            input.className = 'error';
+        } else {
+            input.className = '';
+        }
+    }    
 }
--- 05. DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
+++ 05. DOM Manipulation and Events - Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,37 @@
 function solve() {
-   //TODO...
+   let products = Array.from(document.getElementsByClassName('product'));
+   let checkoutButton = document.querySelector('.checkout');
+   checkoutButton.addEventListener('click', checkout);
+
+   for(let product of products) {
+      product.addEventListener('click', onClick);
+   }
+   
+   let totalPrice = 0;
+   let productsArray = [];
+
+   function onClick(ev) {
+      const productName = ev.currentTarget.querySelector('.product-title').textContent;          
+      const productPrice = Number(ev.currentTarget.querySelector('.product-line-price').textContent);
+      let textArea = document.querySelector('textarea');
+
+      if (!productsArray.includes(productName)) {
+         productsArray.push(productName);
+      }
+
+      textArea.textContent += `Added ${productName} for ${productPrice.toFixed(2)} to the cart.\n`
+
+      totalPrice += productPrice;
+   }
+
+   function checkout() {
+      let textArea = document.querySelector('textarea');
+      textArea.textContent += `You bought ${productsArray.join(', ')} for ${totalPrice.toFixed(2)}.`
+
+      for(let product of products) {
+         product.removeEventListener('click', onClick);
+      }
+            
+      checkoutButton.removeEventListener('click', checkout);
+   }
 }
